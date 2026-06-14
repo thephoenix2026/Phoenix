1afea7ae81d23dc2d7e5e5dd12e114ba26430f81
Hazimzaky <haznmz0@gmail.com>
2026-06-14 22:38:13 +0300

    Fix basePath to /Phoenix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/thephoenix2026",
+  basePath: "/Phoenix",
   images: {
     unoptimized: true,
   },
